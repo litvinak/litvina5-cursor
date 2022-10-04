762f5f14c10ceb10f06f552204f5788a0f1a5c36
Karina <litvina.k@ukr.net>
2022-10-04 23:13:51 +0300

    new

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 //Створіть функцію getRandomArray(length, min, max) – яка повертає масив випадкових цілих чисел
-function getRandomArray(length, min, max) {
+function getRandomArray(length) {
     let arr = [];
     for (let i = 0; i < length; i++) {
-        arr.push(Math.floor(Math.random() * (max - min) + min));
+        arr.push(randomNumber(1, 100));
+        randomNumber()
     }
     return arr;
 }
 
-console.log(getRandomArray(15, 1, 100));
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+console.log(getRandomArray(15));
 
 //Створіть функцію getModa(...numbers) – яка вираховує моду всіх переданих в неї аргументів
 function getModa(...numbers) {
@@ -40,14 +45,14 @@ console.log(getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 //Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів.
 
 function getAverage(...numbers) {
-    let i = 0;
-    let result = null;
-    if(Number.isInteger(numbers[i])) {
-            let sum = numbers.reduce((a, b) => a + b, 0);
-            result = sum / numbers.length
-        }
-    return result;
+    let result = 0;
+    for(let i = 0; i < numbers.length; i++) {
+        result += numbers[i];
     }
+    return Math.trunc(result / numbers.length);
+
+    }
+    
     
 
 console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
